48f46f7596c99c61478a7fc578b4955b6b286174
anitahitouch <anitahitouch@gmail.com>
2015-06-11 23:51:58 +0530

    Updated ls,cat and write method

--- src/folders-aws.js
+++ src/folders-aws.js
@@ -1,5 +1,6 @@
 // Folders.io connector to AWS
-var request = require('aws-sdk');
+var AWS = require('aws-sdk');
+var path = require('path');
 
 
 /*
@@ -8,65 +9,85 @@ var request = require('aws-sdk');
  * export AWS_SECRET_ACCESS_KEY='SECRET'
  */
 
-// Be wary of region. Keep in mind many services support "S3" endpoint semantics outside of AWS.
-AWS.config.region = 'us-west-2';
+var s3;
 
+var FoldersAws = function (prefix, options) {
 
+    this.configure(options);
+    this.prefix = prefix || "/http_window.io_0:aws/";
+    s3 = new AWS.S3();
+    console.log("inin foldersAws,", this.bucket);
 
-var baseurl;
-//TODO we may want to pass the host, port, username as the param of inin
-var FoldersAws = function(prefix, options) {
-        this.prefix = prefix;
-
-        baseurl = options.baseurl;
-        if (baseurl.length && baseurl.substr(-1) != "/")
-                baseurl = baseurl + "/";
-        this.username = options.username;
-        console.log("inin foldersAws,", baseurl, this.username);
 };
 
+FoldersAws.prototype.configure = function (options) {
 
-
-var s3 = new AWS.S3();
-s3.listBuckets(function(err, data) {
-  if (err) { console.log("Error:", err); }
-  else {
-    for (var index in data.Buckets) {
-      var bucket = data.Buckets[index];
-      console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+    /*
+     * load credentials from disk file 
+     */
+    if (options.credentialsFilePath) {
+        AWS.config.loadFromPath(options.credentialsFilePath);
     }
-  }
-});
+    /*
+     * hard-code credentials inside an application. 
+     * Use this method only for small personal 
+     * scripts or for testing purposes.
+     */
+    else if (options.accessKeyId && options.secretAccessKey) {
+        AWS.config.update({
+            accessKeyId: options.accessKeyId,
+            secretAccessKey: options.secretAccessKey
+        });
+    }
 
 
-var write = function(uri, stream, cb) {
-        var s3obj = new AWS.S3({params: {Bucket: 'myBucket', Key: 'myKey'}});
-        s3obj.upload({Body: stream}).
-                on('httpUploadProgress', function(evt) { console.log(evt); }).
-                send(function(error, response) {
-                        if (error) {
-                                console.error(error);
-                                return cb(null, error);
-                        }
-                        return cb("created success");
-                });
+    updateRegion();
+
+
+};
+
+var write = function (uri, stream, cb) {
+
+
+    var self = this;
+    var params = {
+        Bucket: 'build.riplet.com',
+        Key: uri,
+        Body: stream
+    };
+    s3.upload(params).
+    on('httpUploadProgress', function (evt) {
+        console.log(evt);
+    }).
+    send(function (error, response) {
+        if (error) {
+				console.error(error);
+				return cb(error, null);
+			}
+        return cb(null, "created success");
+    });
 };
 
 
-var cat = function(path, cb) {
-        var s3 = new AWS.S3();
-        var params = {Bucket: 'myBucket', Key: 'myImageFile.jpg'};
-        var file = require('fs').createWriteStream('/path/to/file.jpg');
-        // FIXME: See if we can get some info on the remote file, esp. length.
-// headObject / listObjects  works well enough usually.
-        var body = s3.getObject(params).createReadStream();
-        cb({
-                stream : body,
-                size : fileStatus.length,
-                name : fileStatus.pathSuffix
-        });
-}
+var cat = function (path, bucket, cb) {
 
+    var self = this;
+    var params = {
+        Bucket: bucket,
+        Key: path
+    };
+    // FIXME: See if we can get some info on the remote file, esp. length.
+    // headObject / listObjects  works well enough usually.
+    var body = s3.getObject(params).createReadStream();
+
+    cb(null, {
+        stream: body
+            //size : fileStatus.length,
+            //name : fileStatus.pathSuffix
+    });
+};
+
+/*
 var getUri = function(path, cb) {
         var s3 = new AWS.S3();
         var params = {Bucket: 'myBucket', Key: 'myKey'};
@@ -79,12 +100,301 @@ var params = {Bucket: 'myBucket', Key: 'myKey'};
 var url = s3.getSignedUrl('putObject', params);
 
 }
+*/
 
 module.exports = FoldersAws;
 
-FoldersAws.prototype.ls = function(path,cb){
-        ls(path, cb);
+var updateRegion = function (region) {
+
+    AWS.config.update({
+        region: region || 'us-west-2'
+    });
+
 };
+
+FoldersAws.prototype.ls = function (bucket, prefix, cb) {
+
+    var self = this;
+    prefix = prefix || null;
+
+    var t = function (err, result) {
+
+        if (err) {
+
+            cb(err, null);
+        }
+
+        cb(null, result);
+
+    };
+
+    if (!self.flag && bucket instanceof Array) {
+
+        self.lsBuckets(bucket, prefix, t);
+
+    } else if (!self.flag && typeof bucket == 'string') {
+
+        self.lsBucket(bucket, prefix, '', t);
+    } else if ((!self.flag && !bucket) || self.flag == 'ALL_BUCKETS') {
+
+        listBuckets(cb);
+            //getBucketsLocation(['mybucket.test.com','mybucket3.test.com'])	
+
+    }
+
+
+
+};
+
+
+/* This function takes 
+ *
+ *
+ */
+
+var getBucketsLocation = function (buckets) {
+
+    if (!(buckets instanceof Array))
+        buckets = new Array(buckets);
+
+    var tasksToGo = buckets.length;
+    for (var i = 0; i < buckets.length; ++i) {
+
+        var params = {
+            Bucket: buckets[i] /* required */
+        };
+        s3.getBucketLocation(params, function (err, data) {
+            if (err)
+                console.log(err, err.stack); // an error occurred
+            else {
+                result[buckets[tasksToGo - 1]] = data.LocationConstraint
+                if (--tasksToGo == 0)
+                    return;
+                //cb(null,result); 
+            } // successful response
+        });
+
+    }
+
+}
+
+FoldersAws.prototype.test = function () {
+    var bucket = 'mybucket4.test.com';
+    var prefix = null;
+
+
+}
+
+/*
+ * returns all buckets 
+ * associated with this 
+ * credentials
+ */
+var listBuckets = function (cb) {
+
+    s3.listBuckets(function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            cb(err, null);
+        } // an error occurred
+        else {
+            bucket = data.Buckets.map(function (item) {
+                return (item.Name);
+            })
+            cb(null, bucket);
+        }
+    });
+
+}
+
+FoldersAws.prototype.lsBucket = function (bucket, prefix, pathPrefix, cb) {
+
+        var result, self = this;
+
+
+        s3.getBucketLocation({
+            Bucket: bucket
+        }, function (err, data) {
+            if (err) {
+                console.log(err, err.stack);
+            } // an error occurred
+            else {
+
+                updateRegion(data.LocationConstraint);
+
+                s3.listObjects({
+                    Bucket: bucket,
+                    Prefix: prefix
+                }, function (err, data) {
+
+                    if (err) {
+                        console.log("error occured in folders-aws lsBucket() ", err);
+                        return cb(err, null);
+
+                    } else {
+
+                        result = self.asFolders(pathPrefix, data.Contents);
+                        return cb(null, result);
+
+                    }
+                })
+
+            }
+        });
+
+    }
+    /*
+     * This function resturns list of objects 
+     * in array of buckets . takes callback ,prefix
+     * and bucket list as inputs
+     *
+     */
+
+FoldersAws.prototype.lsBuckets = function (buckets, prefix, cb) {
+    var self = this;
+    var result = [];
+    var tasksToGo = buckets.length;
+
+
+    if (0 == tasksToGo) {
+
+        return cb(null, result);
+
+    }
+    for (var i = 0; i < buckets.length; ++i) {
+
+        self.listBucket(buckets[i], prefix, buckets[i] + '/', function (err, data) {
+
+
+            if (err) {
+                console.log("error occured in folders-aws lsBuckets() ", err);
+                return cb(err, null);
+
+            } else {
+
+                result = result.concat(data);
+
+                if (--tasksToGo == 0) {
+
+                    return cb(null, result);
+
+                }
+
+            }
+
+
+        });
+
+        /*
+		
+		s3.listObjects({Bucket:buckets[i],Prefix:prefix},function(err,data){
+
+				if (err){
+					console.log("error occured in folders-aws lsBuckets() ",err)
+					return cb(err,null)
+					
+				}
+				else{
+					
+					result = result.concat(self.asFolders(buckets[tasksToGo-1],data.Contents))
+						console.log(result)
+					
+					if (--tasksToGo == 0 ){
+						
+						return cb(null,result)
+						
+					}
+					
+				}
+		})
+		*/
+    }
+
+
+};
+
+/*
+ * Recursive version of lsBuckets
+ * not usefull 
+ *
+var lsBuckets = function(buckets,prefix,i,cb,result){
+	
+	if (i >= buckets.length){
+			return cb(null,result)
+	}
+		
+	s3.listObjects({Bucket:buckets[i],Prefix:prefix},function(err,data){
+		
+		if (err){
+			
+			return cb(err,null)
+			
+		}
+		
+		else{
+			
+			result = result.concat(data.Contents)
+			
+			lsBuckets(buckets,prefix,++i,cb,result)
+		}
+		
+	})
+	
+}
+*/
+
+/*
+ * If they pass an array of bucket names,
+ *  then we'd use those as the root folder names
+ */
+
+FoldersAws.prototype.asFolders = function ( /*prefix,*/ pathPrefix, files) {
+    var out = [];
+    /*
+  if (prefix){	  
+	  if ((prefix.length-1) != prefix.lastIndexOf('/'))
+	  prefix += '/'	  
+  }
+  */
+
+    /*
+     * Excluding the prefix if it  is 
+     * directory.Only listing contents
+     * inside prefix but not the prefix
+     * itself
+     */
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        //if ( prefix == null || file.Key != prefix){
+        var o = {
+            name: file.Key
+        };
+        o.fullPath = pathPrefix + o.name;
+        o.uri = "#" + this.prefix + o.fullPath;
+        o.size = file.Size || 0;
+        o.extension = path.extname(o.name).substr(1, path.extname(o.name).length - 1) || 'DIR';
+        o.type = "text/plain";
+        o.modificationTime = file.LastModified;
+        out.push(o);
+        //}
+
+    }
+    return out;
+
+};
+
+
+
+
+FoldersAws.prototype.features = FoldersAws.features = {
+    cat: true,
+    ls: true,
+    write: true,
+    server: false
+};
+
+
+
 
 //Temporary comment meta, have to fixed the 'viewfs' first
 //FoldersAws.prototype.meta = function(path,files,cb){
@@ -93,45 +403,55 @@ FoldersAws.prototype.ls = function(path,cb){
 //      lsMounts(path, cb);
 //};
 
-FoldersAws.prototype.write = function(data, cb) {
-        var stream = data.data;
-        // var headers = data.headers;
-        var streamId = data.streamId;
-        var shareId = data.shareId;
-        var uri = data.uri;
+FoldersAws.prototype.write = function (key, data, cb) {
+    //var stream = data.data;
+    // var headers = data.headers;
+    //var streamId = data.streamId;
+    //var shareId = data.shareId;
+    //var uri = data.uri;
 
+    /*
         var headers = {
                 "Content-Type" : "application/json"
         };
 
-        write(uri, stream, function(result,error) {
-                if (error){
-                        cb(null, error);
-                        return;
-                }
+		*/
+    var uri = key;
+    write(uri, data, function (error, result) {
+        if (error) {
 
+            cb(error, null);
+            return;
+        }
+
+        cb(null, result);
+            /*
                 cb({
                         streamId : streamId,
                         data : result,
                         headers : headers,
                         shareId : shareId
                 });
-        });
+				*/
+    });
 
 };
 
-FoldersAws.prototype.cat = function(data, cb) {
-        var path = data;
 
-        cat(path, function(result, error) {
-        cat(path, function(result, error) {
 
-                if (error){
-                        cb(null, error);
-                        return;
-                }
 
-                cb(result);
+FoldersAws.prototype.cat = function (key, cb) {
+    var path = key,
+        self = this;
+    cat(path, self.bucket, function (err, result) {
+
+        if (err) {
+            console.log("error in folders-aws cat() ", err)
+            cb(err, null);
+            return;
+        }
+
+        cb(null, result);
 
         //              var headers = {
         //                      "Content-Length" : result.size,
@@ -147,6 +467,5 @@ FoldersAws.prototype.cat = function(data, cb) {
         //                      headers : headers,
         //                      shareId : data.shareId
         //              });
-        });
+    });
 };
-
